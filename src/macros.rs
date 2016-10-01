86e2f908fb6ca8223ae92445349d78411702002d
Paul Stansifer <paul.stansifer@gmail.com>
2016-09-30 22:58:32 -0400

    Reformat and remove a redundant macro definition.

--- src/macros.rs
+++ src/macros.rs
@@ -396,37 +396,6 @@ macro_rules! get_form {
 }
 
 macro_rules! Reifiable {
-    // HACK: everything is parameterized over 't...
-    (() $(pub)* struct $name:ident/*<'t>*/ { $($field:ident : $t:ty),* }) => {
-        impl<'t> ::runtime::reify::Reifiable<'t> for $name {
-            fn ty() -> ::ast::Ast<'static> {
-                ast! ({ get_form!("type", "struct") ;
-                    "component_name" => [@"c" $( 
-                        (, (::ast::Ast::VariableReference(n(stringify!($field))))) ),* ],
-                    "component" => [@"c" $( (, (<$t as ::runtime::reify::Reifiable>::ty())) ),*]
-                })
-            }
-            
-            fn type_name() -> Name<'static> { n(stringify!($name)) }
-            
-            fn reify(&self) -> ::runtime::eval::Value<'t> {
-                ::runtime::eval::Struct(assoc_n!( 
-                    $( (stringify!($field)) => self.$field.reify()),* ))
-            }
-            
-            fn reflect(v: &::runtime::eval::Value<'t>) -> Self {
-                extract!((v; ::runtime::eval::Struct) (ref env) => 
-                    $name {
-                        $( $field : 
-                            <$t as ::runtime::reify::Reifiable>::reflect(
-                                env.find(&n(stringify!($field))).unwrap())),*
-                    })
-            }
-        }
-    }
-}
-
-macro_rules! Reifiable {
     /* struct */
     // HACK: everything is parameterized over 't...
     (() $(pub)* struct $name:ident/*<'t>*/ { $($field:ident : $t:ty),* }) => {
@@ -460,8 +429,11 @@ macro_rules! Reifiable {
     
     // The `$((...))*` and `$(<...>)*` patterns deal with the fact that the `()` and `<>` 
     //  might be completely absent (the `*` matches 0 or 1 times)
-    (() $(pub)* enum $name:ident$(<$($t_p:ident),*>)*/*<'t>*/ { $($choice:ident$(( $($part:ty),* ))*),* }) => {
-        impl<'t $($(, $t_p : Reifiable<'t>)*)*> ::runtime::reify::Reifiable<'t> for $name<$($($t_p),*)*> {
+    (() $(pub)* enum $name:ident$(<$($t_p:ident),*>)*/*<'t>*/ { 
+        $($choice:ident$(( $($part:ty),* ))*),* 
+    }) => {
+        impl<'t $($(, $t_p : Reifiable<'t>)*)*> ::runtime::reify::Reifiable<'t> 
+                for $name<$($($t_p),*)*> {
             fn ty() -> ::ast::Ast<'static> {
                 ast! ({ get_form!("type", "enum") ;
                     "name" => [@"c" $( 
@@ -474,12 +446,15 @@ macro_rules! Reifiable {
             
             fn type_name() -> Name<'static> { n(stringify!($name)) }
             
+            #[allow(unused_mut)] // rustc bug! `v` has to be mutable, but it complains
             fn reify(&self) -> ::runtime::eval::Value<'t> {
                 match self { $(
-                    choice_pat!( ( $($($part),*)* ) (a b c d e f g h i j k l m n o p q r s t) $name::$choice ; ())
+                    choice_pat!( ( $($($part),*)* ) (a b c d e f g h i j k l m n o p q r s t) 
+                                 $name::$choice ; ())
                     => {
-                        let mut v = vec![]; // How does this not need to be mut!! TODO!!
-                        choice_vec!( ( $($($part),*)* ) (a b c d e f g h i j k l m n o p q r s t) v);
+                        let mut v = vec![];
+                        choice_vec!( ( $($($part),*)* ) (a b c d e f g h i j k l m n o p q r s t) 
+                                     v);
                         ::runtime::eval::Value::Enum(n(stringify!($choice)), v)
                     }
                 ),* }
@@ -490,15 +465,6 @@ macro_rules! Reifiable {
                     make_enum_reflect!(choice; parts; $name$(<$($t_p),*>)*/*<'t>*/ 
                         { $($choice $(( $($part),* ))*),* } )
                 })
-
-                /*
-                extract!((v; ::runtime::eval::Enum) (ref choice, ref parts) => 
-                    match choice.orig {
-                        $( stringify!($choice) => { 
-                            unpack_parts!( ( $($($part),*)* ) parts; 0; constructor; ())
-                        }, )* 
-                        _ => { panic!("ICE: invalid enum choice: {:?}", choice) }
-                    })*/
             }
         }
     }
@@ -538,10 +504,12 @@ macro_rules! choice_vec {
 //  so we use this to manually walk over the choices
 macro_rules! make_enum_reflect {
     ($choice_name:ident; $parts_name:ident; $name:ident$(<$($t_p:ident),*>)*/*<'t>*/ { 
-        $choice_car:ident $(( $($part_cars:ty),* ))* $(, $choice_cdr:ident$(( $($part_cdr:ty),* ))*)* }) => {
-            
+        $choice_car:ident $(( $($part_cars:ty),* ))* 
+        $(, $choice_cdr:ident$(( $($part_cdr:ty),* ))*)* 
+    }) => {    
         if $choice_name.is(stringify!($choice_car)) {
-            unpack_parts!( $(( $($part_cars),* ))* $parts_name; 0; $name::$choice_car$(::< $($t_p),* >)*; ())
+            unpack_parts!( $(( $($part_cars),* ))* $parts_name; 0; 
+                           $name::$choice_car$(::< $($t_p),* >)*; ())
         } else {
             make_enum_reflect!($choice_name; $parts_name; $name$(<$($t_p),*>)*/*<'t>*/ {
                 $($choice_cdr $(( $($part_cdr),* ))* ),* })
