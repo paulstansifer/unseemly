0d6c140197d4399b40aeeb603eefd44f16c03dbb
Paul Stansifer <paul.stansifer@gmail.com>
2021-04-29 22:08:14 -0400

    Allow `deprecated`, until the transition to `Sky` is complete.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@
 // You shouldn't write code in Unseemly.
 // Instead, you should implement your programming language as Unseemly macros.
 
-#![allow(dead_code, unused_macros, non_snake_case, unused_imports, non_upper_case_globals)]
+#![allow(
+    dead_code,
+    unused_macros,
+    non_snake_case,
+    unused_imports,
+    non_upper_case_globals,
+    deprecated
+)]
 // dead_code and unused_macros are hopefully temporary allowances
 // non_snake_case is stylistic, unused_imports is inaccurate for `cargo check`
 // non_upper_case_globals is stylistic; I like my thread_local!s lowercase.
+// deprecated is temporary, until `Sky` replaces `EnvMBE` (and the deprecated calls are cleaned up)
 #![recursion_limit = "128"] // Yikes.
 
 // for testing; requires `cargo +nightly`
